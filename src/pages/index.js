1b021fe5e7eb121594f2cc71905e1d084b76d3e4
jeet <jeetkacha13@gmail.com>
2023-05-31 00:58:00 +0530

    added link to download resume

--- src/pages/index.js
+++ src/pages/index.js
@@ -117,13 +117,15 @@ export default function Home() {
                   })}
                 </div>
                 <div className="p-4">
-                  <button
+                  <a
+                    href="https://drive.google.com/file/d/1hKJcMPmPEaJXLVKP4qJl8LOLhdJHPaOj/view"
+                    target="_blank"
                     type="button"
                     class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white border border-gray-200 hover:bg-gray-100 hover:text-gray-900-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 flex items-center w-full justify-center"
                   >
                     <span className="mr-2">Download Resume</span>
                     <Download size={15} />
-                  </button>
+                  </a>
                 </div>
               </div>
             </div>
